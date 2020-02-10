e1f3db2a8d3bf32779fe73fd90e652e0be576512
Amilton Leme <amilton.leme@gmail.com>
2020-02-10 20:23:48 -0300

    Criando operação de instanciação

--- src/main/java/com/amiltonleme/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/amiltonleme/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,5 @@
 package com.amiltonleme.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,7 +21,7 @@ public class CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
 		Categoria obj = service.find(id);
-		return ResponseEntity.ok(obj);
+		return ResponseEntity.ok().body(obj);
 		
 	}
 	
