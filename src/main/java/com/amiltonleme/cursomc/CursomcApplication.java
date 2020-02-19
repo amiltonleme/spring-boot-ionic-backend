37398dde8b9b82b749c321933aecca273af31166
Amilton Leme <amilton.leme@gmail.com>
2020-02-18 21:04:51 -0300

    Estado e Cidade

--- src/main/java/com/amiltonleme/cursomc/CursomcApplication.java
+++ src/main/java/com/amiltonleme/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.amiltonleme.cursomc.domain.Categoria;
+import com.amiltonleme.cursomc.domain.Cidade;
+import com.amiltonleme.cursomc.domain.Estado;
 import com.amiltonleme.cursomc.domain.Produto;
 import com.amiltonleme.cursomc.repositories.CategoriaRepository;
+import com.amiltonleme.cursomc.repositories.CidadeRepository;
+import com.amiltonleme.cursomc.repositories.EstadoRepository;
 import com.amiltonleme.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -23,6 +27,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -46,12 +54,29 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		
-		//Salvar os dados no banco (Repository)
+		//Salvar os dados no banco (Repository) Categoria / Produto
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
+		//+++++++++++++++++++++++++++++++++++++++++++++++++++
 		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "Sâo Paulo");
+		
+		//Quando se tem muitos pra um, a associação já é feita no construtor
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		// Associações de Estado com as cidades
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est1.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		
+		//Salvar os dados no banco (Repository) Estado / Cidade
+		//Como o Estado tem várias cidades, ele será inserido primeiro.
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));;
 		
 	}
 
--- src/main/java/com/amiltonleme/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/amiltonleme/cursomc/repositories/ProdutoRepository.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Repository;
 
 import com.amiltonleme.cursomc.domain.Produto;
 
+// Classe que faz comunicação com o banco de dados. (Repository).
+
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 	
