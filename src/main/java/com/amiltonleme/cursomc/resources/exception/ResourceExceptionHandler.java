916fd19ec6d673fbae4e969c82d9f706cb076880
Amilton Leme <amilton.leme@gmail.com>
2020-02-28 21:34:43 -0300

    Deletando uma Categoria e tratando exception

--- src/main/java/com/amiltonleme/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/amiltonleme/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.amiltonleme.cursomc.resources.exception;
 
+import java.security.Timestamp;
+import java.text.SimpleDateFormat;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
@@ -7,16 +10,24 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.amiltonleme.cursomc.services.exceptions.DataIntegrityException;
 import com.amiltonleme.cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 	
-	@ExceptionHandler(ObjectNotFoundException.class	)
+	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
 			
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
+			
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 	
 
--- src/main/java/com/amiltonleme/cursomc/services/CategoriaService.java
+++ src/main/java/com/amiltonleme/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.amiltonleme.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.amiltonleme.cursomc.domain.Categoria;
 import com.amiltonleme.cursomc.repositories.CategoriaRepository;
+import com.amiltonleme.cursomc.services.exceptions.DataIntegrityException;
 import com.amiltonleme.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -45,6 +47,16 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id){
+		find(id);
+		try {
+		repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e){
+			throw new DataIntegrityException("Não é possível excluir uma categoria que contenha produtos");
+		}
 	}
 	
 }
