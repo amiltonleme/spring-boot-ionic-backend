b8716b72a5f90144626821889909a19695f08123
Amilton Leme <amilton.leme@gmail.com>
2020-03-02 20:21:45 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/amiltonleme/cursomc/services/ClienteService.java
+++ src/main/java/com/amiltonleme/cursomc/services/ClienteService.java
@@ -9,10 +9,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.amiltonleme.cursomc.domain.Cidade;
 import com.amiltonleme.cursomc.domain.Cliente;
+import com.amiltonleme.cursomc.domain.Endereco;
+import com.amiltonleme.cursomc.domain.enums.TipoCliente;
 import com.amiltonleme.cursomc.dto.ClienteDTO;
+import com.amiltonleme.cursomc.dto.ClienteNewDTO;
 import com.amiltonleme.cursomc.repositories.ClienteRepository;
+import com.amiltonleme.cursomc.repositories.EnderecoRepository;
 import com.amiltonleme.cursomc.services.exceptions.DataIntegrityException;
 import com.amiltonleme.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -21,6 +27,9 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 // Implementação utlilizada para Spring Boot 2.x.x em diante.
 	public Cliente find(Integer id) {
@@ -38,6 +47,17 @@ public class ClienteService {
 //		
 //		return obj.orElse(null);
 	}
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		/* ao se setar ID com NULL se garante que será criado um novo objeto. Caso
+		contrário, se houver um ID especificado será atualizado o objeto
+		correspondente.*/
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return repo.save(obj);
+	}
+
 	
 	public Cliente update(Cliente obj) {
 		/* Se houver um ID especificado será atualizado o objeto
@@ -72,6 +92,21 @@ public class ClienteService {
 		
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cid, cli);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if (objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if (objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
 	}
 	
 	//Private porque é um método auxiliar dentro da classe
