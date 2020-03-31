3e4e24a8e4f696fe96ce438f8d15f1b7ed271d59
Amilton Leme <amilton.leme@gmail.com>
2020-03-30 22:39:12 -0300

    Email HTML

--- src/main/java/com/amiltonleme/cursomc/services/EmailService.java
+++ src/main/java/com/amiltonleme/cursomc/services/EmailService.java
@@ -1,13 +1,19 @@
 package com.amiltonleme.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.amiltonleme.cursomc.domain.Pedido;
 
 public interface EmailService {
 	
-	void senderOrderConfirmationEmail(Pedido obj);
+	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg); 
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 
 }
