5b49da25b883a773db022a7698c5f3e6d3b133f6
Amilton Leme <amilton.leme@gmail.com>
2020-04-01 22:08:43 -0300

    Configuração inicial do Spring Security

---
+++ src/main/java/com/amiltonleme/cursomc/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.amiltonleme.cursomc.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private Environment env;
+	
+	private static final String[] PUBLIC_MATCHERS = {
+		"/h2-console/**",
+	};
+	
+	private static final String[] PUBLIC_MATCHERS_GET = {
+			"/produtos/**",
+			"/categorias/**"
+		};
+	
+	@Override
+	protected void configure (HttpSecurity http) throws Exception {
+		//Se a configuração estiver em test a aplicação rodará normal.
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+		//Permite os argumentos do vetor PUBLIC_MATCHERS e pede autenticação dos outros
+		http.cors().and().csrf().disable();
+		http.authorizeRequests()
+		//Pode recuperar os argumentos de PUBLIC_MATCHERS_GET, porém não pode alterar
+		.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+		//Pode recuperar e alterar os argumentos de PUBLIC_MATCHERS.
+		.antMatchers(PUBLIC_MATCHERS).permitAll()
+		.anyRequest().authenticated();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+	    return source;
+	}
+}
