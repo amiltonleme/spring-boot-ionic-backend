acd4435d0c43ed2ca9fa75a1f297ce5c2ae22c12
Amilton Leme <amilton.leme@gmail.com>
2020-04-03 23:58:45 -0300

    Implementando autenticação e geração do token JWT

---
+++ src/main/java/com/amiltonleme/cursomc/security/UserSS.java
@@ -0,0 +1,73 @@
+package com.amiltonleme.cursomc.security;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.amiltonleme.cursomc.domain.enums.Perfil;
+
+public class UserSS implements UserDetails{
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String email;
+	private String senha;
+	private Collection<? extends GrantedAuthority> authorities;
+	
+	public UserSS(){
+		
+	}
+	
+	public UserSS(Integer id, String email, String senha, Set<Perfil> perfis) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.senha = senha;
+		//Para cada Perfil x na lista de perfis eu vou dar um new SimpleGrantedAuthority(x.getDescricao()) 
+		this.authorities = perfis.stream().map(x -> new SimpleGrantedAuthority(x.getDescricao())).collect(Collectors.toList());
+		}
+
+	public Integer getId() {
+		return id;
+	} 
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		return senha;
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+}
