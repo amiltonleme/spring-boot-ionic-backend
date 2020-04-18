e1c459e909dc542629f82b1704e2f5cd50a8a7e4
Amilton Leme <amilton.leme@gmail.com>
2020-04-17 22:33:51 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/amiltonleme/cursomc/CursomcApplication.java
+++ src/main/java/com/amiltonleme/cursomc/CursomcApplication.java
@@ -1,19 +1,26 @@
 package com.amiltonleme.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.amiltonleme.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+		
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		s3Service.uploadFile("C:\\ws-spring-boot-ionic-nelio-alves\\Imagens\\20200414_183156.jpg");
 	}
 
 }
